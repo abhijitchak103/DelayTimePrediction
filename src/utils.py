69120ccebd2a32075cf1981ddeb75b309cbee98f
abhijitchak103 <abhijityachak@gmail.com>
2023-04-21 18:17:20 +0530

    Added functuionalities to utils.py

--- src/utils.py
+++ src/utils.py
@@ -28,7 +28,7 @@ def deg2rad(deg):
         raise CustomException(e, sys)
 
 
-def getDistancefromLatLonginKm(lat1, lon1, lat2, lon2):
+def get_distance_from_latlong_in_km(lat1, lon1, lat2, lon2):
     """
     Returns the Distance in Kms upto 2 decimal places between 2 points with Latitude and Longitude.
     --------------------------------------
@@ -58,3 +58,181 @@ def getDistancefromLatLonginKm(lat1, lon1, lat2, lon2):
     except Exception as e:
         logging.info('Error occured in utils.getDistancefromLatLonginKm')
         raise CustomException(e, sys)
+    
+
+def preprocess_date_columns(df: pd.DataFrame, columns: list) -> pd.DataFrame:
+    """
+    Cleans the input pd.DataFrame columns and returns the updated pd.DataFrame
+    -----------------------------------------
+
+    Arguments:
+    ----------
+    df: Pandas DataFrame
+    columns: Columns to Clean
+    -----------------------------------------
+
+    Returns:
+    --------
+    df: Pandas DataFrame
+    """
+    try:
+        df[columns[0]] = pd.to_datetime(df[columns[0]])
+        logging.info('Changed Order_Date to datetime object')
+
+        time_ordered = list(df[columns[1]])
+        time_picked = list(df[columns[2]])
+
+        df['Time_Orderd_num'] = transform_time_columns(time_ordered)
+        df['Time_Order_picked_num'] = transform_time_columns(time_picked)
+        logging.info('Created temporary numerical columns from Time_orderd and Time_Order_picked')
+
+        df = df.mask(df == '')
+        df.dropna(axis=0, inplace=True)
+        logging.info('Dropped null rows in new columns')
+
+
+        df['Time_to_pick'] = (df['Time_Order_picked_num'] - df['Time_Orderd_num'])*60
+        df['Time_to_pick'] = np.around(df['Time_to_pick'].astype(np.double),3)
+        logging.info('Created new column Time_to_pick')
+
+        df.drop(columns = ['Time_Orderd_num', 'Time_Order_picked_num', 'Time_Order_picked'], axis = 1, inplace = True)
+        logging.info('Dropped temporary columns and Time_Order_picked column')
+
+        df['Month'] = df[columns[0]].dt.month
+        df.drop(columns = columns[0], axis = 1, inplace = True)
+        logging.info('Created new Month feature from Order_Date and dropped Order_Date')
+
+        return df
+    except Exception as e:
+        logging.info('Error occured in utils.preprocessDataColumns')
+        raise CustomException(e, sys)
+
+
+def transform_time_columns(list_to_clean: list) -> list:
+    """
+    Returns a modified list
+    -----------------------
+    
+    Arguments:
+    ----------
+    list_to_clean: list
+    -----------------------
+
+    Returns:
+    --------
+    list
+    """
+    try:
+
+        # Cleaning data not having ':' because of irrelevant information 
+        for i, x in enumerate(list_to_clean):
+            if ':' not in str(x):
+                list_to_clean[i] = ''
+        
+        # Cleaning data for typos
+        for i, x in enumerate(list_to_clean):
+            if x.count(':') > 1:
+                list_to_clean[i]= x[:5]
+        
+        # Getting the time in hours for calculation 
+        for i, x in enumerate(list_to_clean):
+            if x != '':
+                hour = float(x[:2])
+                mins = float(x[3:])
+
+                list_to_clean[i] = round(hour + mins/60, 2)
+                
+        return list_to_clean
+    except Exception as e:
+        logging.info('Error Occured in utils.transform time columns')
+        raise CustomException(e, sys)
+    
+
+def get_part_of_day(df: pd.DataFrame, column: str) -> pd.DataFrame:
+    """
+    Creates a new column Time_of_Day_Ordered which holds the part of day of order
+    
+    Arguments:
+    ----------
+    df: pandas DataFrame
+    column: column name to extract information from
+
+    Returns:
+    --------
+    df: pandas DataFrame
+    """
+    try:    
+        time_of_day = list(df[column])
+
+        for i, x in enumerate(time_of_day):
+            time_of_day[i] = float(x[:2]) + float(x[3:])/60
+            
+        for i, x in enumerate(time_of_day):
+            if 0 < x <= 6:
+                time_of_day[i] = 'Early Morning'
+            elif 6 < x <= 12:
+                time_of_day[i] = 'Morning'
+            elif 12 < x <= 18:
+                time_of_day[i] = 'Evening'
+            elif 18 < x <= 24:
+                time_of_day[i] = 'Night'
+                
+        df['Time_of_Day_Ordered'] = time_of_day
+        df.drop(columns = column, axis = 1, inplace = True)
+        return df
+    except Exception as e:
+        logging.info('Error occured in utils.get_part_of_day')
+        raise CustomException(e, sys)
+    
+
+def preprocess_df(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Returns a modified DataFrame
+    
+    Arguments:
+    ----------
+    df: pd.DataFrame
+
+    Returns:
+    --------
+    df: pd.DataFrame
+    """
+    try:
+        df.drop(columns='ID', axis=1, inplace=True)
+        logging.info('Dropped ID column')
+
+        df['Delivery_distance'] = df.apply(
+        lambda x: get_distance_from_latlong_in_km(x['Restaurant_latitude'], 
+                                                x['Restaurant_longitude'], 
+                                                x['Delivery_location_latitude'], 
+                                                x['Delivery_location_longitude']), axis=1)
+        logging.info("Created Column 'Delivery_distance'")
+        
+        columns_to_drop = ['Restaurant_latitude', 'Restaurant_longitude', 
+                            'Delivery_location_latitude', 'Delivery_location_longitude']
+        df.drop(columns = columns_to_drop, axis=1, inplace=True)
+        logging.info('Dropped Latitude and Longitude columns successfully')
+
+        q = df['Delivery_distance'].quantile(0.99)
+        df = df[df['Delivery_distance'] < q ]
+        logging.info("Removed outliers from 'Delivery_distance'")
+
+        logging.info('Preprocessing df to clean Datetime columns')
+        df = preprocess_date_columns(df=df, columns=['Order_Date', 'Time_Orderd', 'Time_Order_picked'])
+        logging.info('Preprocessing df completed')
+
+
+        df = get_part_of_day(df=df, column='Time_Orderd')
+        logging.info('Created Time_of_day_Ordered feature and dropped Time_Orderd')
+
+        cities = list(df['Delivery_person_ID'])
+        cities = [x[:x.find('RES')] for x in cities]
+
+        df['City'] = cities
+        df.drop(columns = 'Delivery_person_ID', axis = 1, inplace = True)
+        logging.info('Created City feature for City of Order. Dropped Delivery_person_ID')
+
+        return df
+    except Exception as e:
+        logging.info('Error Occcured in utils.preprocess_df')
+        raise CustomException

f2d5f118ab2fbfeed13aed4a30993d4727df1a5d
abhijitchak103 <abhijityachak@gmail.com>
2023-04-21 18:26:58 +0530

    Complete initiate_data_ingestion

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -31,10 +31,28 @@ class DataIngestion:
             df = preprocess_df(df=df)
             logging.info('Successfully preprocessed DataFrame')
 
-            
-            
+            os.makedirs(os.path.dirname(self.data_ingestion_config.raw_data_path), exist_ok=True)
+            df.to_csv(self.data_ingestion_config.raw_data_path, index=False)
 
+            logging.info('Successfully saved raw clean data')
 
+            logging.info('Train test split Started')
+
+            train_set, test_set = train_test_split(df, test_size=0.2, random_state=1)
+
+            train_set.to_csv(self.data_ingestion_config.train_data_path, header=True, index=False)
+            logging.info('Successfully saved train data')
+
+            test_set.to_csv(self.data_ingestion_config.test_data_path, header=True, index=False)
+            logging.info('Successfully saved test data')
+
+            logging.info('Train test split complete')
+
+            return(
+                self.data_ingestion_config.train_data_path,
+                self.data_ingestion_config.test_data_path
+            )            
+        
         except Exception as e:
             logging.info('Error occured in DataIngestion.initiate_data_ingestion')
             raise CustomException(e, sys)
